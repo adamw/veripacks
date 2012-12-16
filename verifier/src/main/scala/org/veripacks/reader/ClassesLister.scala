ed49c9e99ee558a6217bdc62c1effcf6bea9395a
adamw <adam@warski.org>
2012-12-16 11:03:56 +0100

    Classes lister

---
+++ verifier/src/main/scala/org/veripacks/reader/ClassesLister.scala
@@ -0,0 +1,90 @@
+package org.veripacks.reader
+
+import collection.mutable
+import java.net.{URL, URLDecoder}
+import java.util.jar.JarFile
+import java.io.File
+import com.typesafe.scalalogging.slf4j.Logging
+import scala.collection.JavaConverters._
+import org.veripacks.ClassName
+
+class ClassesLister extends Logging {
+  // http://stackoverflow.com/questions/1456930/how-do-i-read-all-classes-from-a-java-package-in-the-classpath/7461653#7461653
+  def list(packageName: String): Iterable[ClassName] = {
+    val classLoader = Thread.currentThread().getContextClassLoader
+    val names = new mutable.HashSet[ClassName]()
+
+    val packageNameSlashed = packageName.replace('.', '/')
+    val packageURLs = classLoader.getResources(packageNameSlashed)
+
+    for (packageURL <- packageURLs.asScala) {
+      logger.debug(s"Processing package URL $packageURL")
+      val protocol = packageURL.getProtocol
+
+      if (protocol.equals("jar")) {
+        names ++= allClassesInJar(packageURL, packageNameSlashed)
+      } else if (protocol.equals("file")) {
+        names ++= allClassesInDirectory(packageName, new File(packageURL.getFile))
+      } else {
+        throw new IllegalArgumentException(s"Unsupported protocol $protocol in URL $packageURL for package $packageName.")
+      }
+    }
+
+    names
+  }
+
+  private def allClassesInJar(packageURL: URL, packageNameSlashed: String) = {
+    val names = mutable.HashSet[ClassName]()
+
+    val protocolLength = packageURL.getProtocol.length + 2 // we have to account for :/
+    // build jar file name, then loop through zipped entries
+    val jarFileNameWithProtocol = URLDecoder.decode(packageURL.getFile, "UTF-8")
+    val jarFileName = jarFileNameWithProtocol.substring(protocolLength, jarFileNameWithProtocol.indexOf("!"))
+    val jf = new JarFile(jarFileName)
+    val jarEntries = jf.entries()
+    while (jarEntries.hasMoreElements) {
+      val entryNameSlashed = jarEntries.nextElement().getName
+      if (entryNameSlashed.startsWith(packageNameSlashed)) {
+        addEntryIfClass(names, None, entryNameSlashed)
+      }
+    }
+
+    names.toSet
+  }
+
+  private def allClassesInDirectory(packageName: String, dir: File): Set[ClassName] = {
+    val names = mutable.HashSet[ClassName]()
+    val content = dir.listFiles
+    for (actual <- content) {
+      val entryName = actual.getName
+      val added = addEntryIfClass(names, Some(packageName), entryName)
+      if (!added) {
+        val entryFile = new File(dir, entryName)
+        if (entryFile.isDirectory) {
+          names ++= allClassesInDirectory(s"$packageName.$entryName", entryFile)
+        } else {
+          logger.debug(s"Not a class or a directory: $packageName for package $entryName")
+        }
+      }
+    }
+
+    names.toSet
+  }
+
+  /**
+   * @return True if the entry was a class and hence was added.
+   */
+  private def addEntryIfClass(names: mutable.Set[ClassName], packageName: Option[String], entryNameSlashed: String) = {
+    if (isClass(entryNameSlashed)) {
+      val entryNameSlashedWithoutDotClass = entryNameSlashed.substring(0, entryNameSlashed.length - 6)
+      val className = packageName.map(_ + '.').getOrElse("") + entryNameSlashedWithoutDotClass.replace('/', '.')
+      names += ClassName(className)
+      logger.debug(s"Adding class $className")
+      true
+    } else {
+      false
+    }
+  }
+
+  private def isClass(entryName: String) = entryName.endsWith(".class")
+}
---
+++ verifier/src/test/scala/org/veripacks/reader/ClassesListerTest.scala
@@ -0,0 +1,30 @@
+package org.veripacks.reader
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import org.veripacks.ClassName
+
+class ClassesListerTest extends FlatSpec with ShouldMatchers {
+  it should "list classes that are on the classpath in a folder" in {
+    // When
+    val result = new ClassesLister().list("org.veripacks.data.t1")
+
+    // Then
+    result should have size (4)
+    result should contain (ClassName("org.veripacks.data.t1.p11.Class112"))
+    result should contain (ClassName("org.veripacks.data.t1.p12.package-info"))
+  }
+
+  it should "list classes that are on the classpath in a jar" in {
+    // Given
+    val pkg = "org.slf4j"
+
+    // When
+    val result = new ClassesLister().list(pkg)
+
+    // Then
+    result.foreach(_.name should startWith (pkg))
+    result should contain (ClassName("org.slf4j.Logger"))
+    result should contain (ClassName("org.slf4j.impl.StaticLoggerBinder"))
+  }
+}
