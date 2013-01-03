3abfca8f55228945c36850d29c140248e6645eb3
adamw <adam@warski.org>
2013-01-03 12:33:25 +0100

    Nice toString for usages

--- verifier/src/main/scala/org/veripacks/ClassUsage.scala
+++ verifier/src/main/scala/org/veripacks/ClassUsage.scala
@@ -1,17 +1,32 @@
 package org.veripacks
 
-case class ClassUsage(cls: ClassName, usedIn: ClassName, detail: ClassUsageDetail)
+case class ClassUsage(cls: ClassName, usedIn: ClassName, detail: ClassUsageDetail) {
+  override def toString = s"Class: ${cls.fullName} cannot be used in ${usedIn.fullName}. Class usage details: $detail."
+}
 
 sealed trait ClassUsageDetail {
   val sourceFileName: String
 }
 
-case class MethodSignatureUsageDetail(sourceFileName: String, methodName: String) extends ClassUsageDetail
-case class MethodBodyUsageDetail(sourceFileName: String, methodName: String, lineNumber: Int) extends ClassUsageDetail
-case class FieldUsageDetail(sourceFileName: String, fieldName: String) extends ClassUsageDetail
-case class ClassSignatureUsageDetail(sourceFileName: String) extends ClassUsageDetail
+case class MethodSignatureUsageDetail(sourceFileName: String, methodName: String) extends ClassUsageDetail {
+  override def toString = s"$methodName signature in $sourceFileName"
+}
+
+case class MethodBodyUsageDetail(sourceFileName: String, methodName: String, lineNumber: Int) extends ClassUsageDetail {
+  override def toString = s"line $lineNumber of $methodName method in $sourceFileName"
+}
+
+case class FieldUsageDetail(sourceFileName: String, fieldName: String) extends ClassUsageDetail {
+  override def toString = s"$fieldName field in $sourceFileName"
+}
+
+case class ClassSignatureUsageDetail(sourceFileName: String) extends ClassUsageDetail {
+  override def toString = s"class signature in $sourceFileName"
+}
 
 case class MultipleUsageDetail(usages: Set[ClassUsageDetail]) extends ClassUsageDetail {
   require(usages.size > 0)
   lazy val sourceFileName: String = usages.iterator.next().sourceFileName // All source file names should be the same
+
+  override def toString = usages.mkString("; ")
 }
