50769cb39e377fecd8cb7bc8bc0eba14e103944d
adamw <adam@warski.org>
2013-01-05 14:08:34 +0100

    Updating to Scala 2.10

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization  := "org.veripacks",
     version       := "0.1-SNAPSHOT",
-    scalaVersion  := "2.10.0-RC5",
+    scalaVersion  := "2.10.0",
     scalacOptions += "",
     licenses      := ("Apache2", new java.net.URL("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil,
     publishTo     <<= (version) { version: String =>
@@ -21,10 +21,10 @@ object BuildSettings {
 object Dependencies {
   val asm           = "org.ow2.asm"               % "asm"                   % "4.1"
 
-  val scalaLogging  = "com.typesafe"              % "scalalogging-slf4j_2.10.0-RC3" % "0.4.0"
+  val scalaLogging  = "com.typesafe"              %% "scalalogging-slf4j"   % "1.0.1"
   val logback       = "ch.qos.logback"            % "logback-classic"       % "1.0.7"
 
-  val scalatest     = "org.scalatest"             % "scalatest_2.10.0-RC3"  % "1.8-B1"    % "test"
+  val scalatest     = "org.scalatest"             %% "scalatest"            % "1.9.1"     % "test"
   val mockito       = "org.mockito"               % "mockito-core"          % "1.9.5"     % "test"
 
   val testing = Seq(scalatest, mockito, logback % "test")
