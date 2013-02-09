911bf0f717d65812dc3ea3b375bcf61dbbcd9691
adamw <adam@warski.org>
2013-02-09 13:31:11 +0100

    New annotations: import/require import

---
+++ annotations/src/main/java/org/veripacks/Import.java
@@ -0,0 +1,12 @@
+package org.veripacks;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PACKAGE)
+public @interface Import {
+    String[]    value()  default {};
+}
