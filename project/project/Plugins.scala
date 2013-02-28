b65d665677d12a0531621c843d0b4f90022d150e
adamw <adam@warski.org>
2013-02-28 09:55:28 +0100

    Adding TC reporting

---
+++ project/project/Plugins.scala
@@ -0,0 +1,8 @@
+import sbt._
+
+object Plugins extends Build {
+  lazy val plugins = Project("plugins", file("."))
+    .dependsOn(
+    uri("git://github.com/guardian/sbt-teamcity-test-reporting-plugin.git#1.3")
+  )
+}
