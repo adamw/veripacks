00c80c958c47c74a35b1305667c708623bcde41a
adamw <adam@warski.org>
2013-03-03 11:06:27 +0100

    Extracting a data trait from the exports test

--- verifier/src/test/scala/org/veripacks/classusageverifier/ClassUsageVerifierExportsTest.scala
+++ verifier/src/test/scala/org/veripacks/classusageverifier/ClassUsageVerifierExportsTest.scala
@@ -4,29 +4,8 @@ import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FlatSpec
 import org.veripacks._
 
-class ClassUsageVerifierExportsTest extends FlatSpec with ShouldMatchers {
+class ClassUsageVerifierExportsTest extends FlatSpec with ShouldMatchers with ClassUsageVerifierTestData {
   import ClassUsageVerifierResult._
-
-  val pkg1 = Pkg("foo.bar.p1")
-  val pkg1_1 = Pkg("foo.bar.p1.pp1")
-  val pkg1_1_1 = Pkg("foo.bar.p1.pp1.ppp1")
-  val pkg1_1_2 = Pkg("foo.bar.p1.pp1.ppp2")
-
-  val pkg2 = Pkg("foo.bar.p2")
-
-  val cls1_in_pkg1 = ClassName(pkg1, "cls1_in_pkg1")
-  val cls2_in_pkg1 = ClassName(pkg1, "cls2_in_pkg1")
-  val cls3_in_pkg1 = ClassName(pkg1, "cls3_in_pkg1")
-
-  val cls1_in_pkg1_1 = ClassName(pkg1_1, "cls1_in_pkg1_1")
-  val cls2_in_pkg1_1 = ClassName(pkg1_1, "cls2_in_pkg1_1")
-
-  val cls1_in_pkg1_1_1 = ClassName(pkg1_1_1, "cls1_in_pkg1_1_1")
-  val cls2_in_pkg1_1_1 = ClassName(pkg1_1_1, "cls2_in_pkg1_1_1")
-
-  val cls1_in_pkg1_1_2 = ClassName(pkg1_1_2, "cls2_in_pkg1_1_2")
-
-  val cls1_in_pkg2 = ClassName(pkg2, "cls1_in_pkg2")
 
   val noExport = Map[Pkg, ExportDef]()
 
