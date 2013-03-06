975be8fd66f380115cd3ae6f923574453231b4e0
adamw <adam@warski.org>
2013-03-06 20:26:57 +0100

    Release 0.3

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import scala.Some
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization  := "org.veripacks",
-    version       := "0.3-SNAPSHOT",
+    version       := "0.3",
     scalaVersion  := "2.10.0",
     // Sonatype OSS deployment
     publishTo <<= version { (v: String) =>
