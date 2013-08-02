dd52982bf725fb6576444be9a688b4cf85284293
adamw <adam@warski.org>
2013-08-02 09:32:08 +0200

    Constructing the class usage filters

--- verifier/src/main/scala/org/veripacks/VeripacksBuilder.scala
+++ verifier/src/main/scala/org/veripacks/VeripacksBuilder.scala
@@ -4,6 +4,7 @@ import org.veripacks.reader.dependencies.ClassDependenciesReader
 import org.veripacks.reader.accessdefinitions.{AccessDefinitionsAccumulator, ClassAccessDefinitionsReader}
 import org.veripacks.reader.MetadataReader
 import org.veripacks.verifier.{Verifier, ClassUsageVerifier}
+import scala.collection.JavaConverters._
 
 @NotVerified
 trait VeripacksBuilder {
@@ -26,9 +27,40 @@ trait VeripacksBuilder {
 
 object VeripacksBuilder {
   private var _customAccessDefinitionsReader: CustomAccessDefinitionsReader = NoOpCustomAccessDefinitionsReader
+  private var _classUsageFilter: ClassUsageFilter = AllUnknownClassUsageFilter
 
   def withCustomAccessDefinitionReader(newCustomAccessDefinitionsReader: CustomAccessDefinitionsReader) = {
     _customAccessDefinitionsReader = newCustomAccessDefinitionsReader
+    this
+  }
+
+  def checkUsagesOfClassesFrom(packagePrefix: String): this.type = {
+    checkUsagesOfClassesFrom(List(packagePrefix))
+    this
+  }
+
+  def checkUsagesOfClassesFrom(packagePrefixes: java.util.Collection[String]): this.type = {
+    checkUsagesOfClassesFrom(packagePrefixes.asScala)
+    this
+  }
+
+  def checkUsagesOfClassesFrom(packagePrefixes: Iterable[String]): this.type = {
+    _classUsageFilter = _classUsageFilter.or(IncludeClassUsageFilter(packagePrefixes))
+    this
+  }
+
+  def doNotCheckUsagesOfClassesFrom(packagePrefix: String): this.type = {
+    doNotCheckUsagesOfClassesFrom(List(packagePrefix))
+    this
+  }
+
+  def doNotCheckUsagesOfClassesFrom(packagePrefixes: java.util.Collection[String]): this.type = {
+    doNotCheckUsagesOfClassesFrom(packagePrefixes.asScala)
+    this
+  }
+
+  def doNotCheckUsagesOfClassesFrom(packagePrefixes: Iterable[String]): this.type = {
+    _classUsageFilter = _classUsageFilter.or(ExcludeClassUsageFilter(packagePrefixes))
     this
   }
 
