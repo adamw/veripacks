527a85726f3b0e03ced22723df997ce564e60c95
adamw <adam@warski.org>
2013-08-05 16:13:29 +0200

    Typo

--- self-test/src/test/scala/org/veripacks/VeripacksSelfTest.scala
+++ self-test/src/test/scala/org/veripacks/VeripacksSelfTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FlatSpec
 
 class VeripacksSelfTest extends FlatSpec with ShouldMatchers {
-  it should "not report and contrainst violations in the veripacks code" in {
+  it should "not report and constraint violations in the veripacks code" in {
     VeripacksBuilder
       .requireImportOf("org.objectweb")
       .build
